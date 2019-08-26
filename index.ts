14082aca40c04cb27c47e6a4588d009ae1bbb05e
cdimitroulas <c.dimitroulas@gmail.com>
2019-08-26 14:31:27 +0100

    Format with prettier

--- index.ts
+++ index.ts
@@ -1,17 +1,19 @@
-'use strict'
+"use strict";
 
-import { init } from './scripts';
+import { init } from "./scripts";
 
 const args = process.argv.slice(2);
 const scriptName = args[0];
 
 if (!scriptName) {
-  console.error('Please pass a script name as an argument to @cdimitroulas/typescript-scripts');
+  console.error(
+    "Please pass a script name as an argument to @cdimitroulas/typescript-scripts"
+  );
   process.exit(1);
 }
 
 switch (scriptName) {
-  case 'init':
+  case "init":
     init();
     process.exit(0);
     break;
@@ -20,4 +22,3 @@ switch (scriptName) {
     console.log(`Unknown script '${scriptName}' called. Did you make a typo?`);
     process.exit(1);
 }
-
--- scripts/init.ts
+++ scripts/init.ts
@@ -8,32 +8,40 @@ const tsConfigPath = path.resolve(appDirectory, "tsconfig.json");
 function initialiseTypescriptApp(): void {
   checkTypescriptIsInstalled();
   createTsConfig();
-};
+}
 
 function checkTypescriptIsInstalled(): void {
   try {
-    require(path.resolve(appDirectory, 'node_modules/typescript'));
+    require(path.resolve(appDirectory, "node_modules/typescript"));
   } catch (_) {
-    console.error("It looks like you're trying to use TypeScript but do not have 'typescript' installed.");
-    console.error("Please install 'typescript' by running 'npm install typescript'");
+    console.error(
+      "It looks like you're trying to use TypeScript but do not have 'typescript' installed."
+    );
+    console.error(
+      "Please install 'typescript' by running 'npm install typescript'"
+    );
     process.exit(1);
   }
 }
 
 function createTsConfig(): void {
   if (tsConfigExists()) {
-    console.log("Your repository already contains a tsconfig.json file. If you would like this @cdimitroulas/typescript-scripts to create a new one for you then please delete it and run the init script again");
+    console.log(
+      "Your repository already contains a tsconfig.json file. If you would like this @cdimitroulas/typescript-scripts to create a new one for you then please delete it and run the init script again"
+    );
     return;
-  };
+  }
 
   const defaultTsConfig = {
-    "compilerOptions": {
-      "target": "ES2016" /* Specify ECMAScript target version: 'ES3' (default), 'ES5', 'ES2015', 'ES2016', 'ES2017','ES2018' or 'ESNEXT'. */,
-      "module": "commonjs" /* Specify module code generation: 'none', 'commonjs', 'amd', 'system', 'umd', 'es2015', or 'ESNext'. */,
-      "outDir": "dist" /* Redirect output structure to the directory. */,
-      "noEmitOnError": true /* Do not emit outputs. */,
-      "strict": true /* Enable all strict type-checking options. */,
-      "esModuleInterop": true /* Enables emit interoperability between CommonJS and ES Modules via creation of namespace objects for all imports. Implies 'allowSyntheticDefaultImports'. */
+    compilerOptions: {
+      target:
+        "ES2016" /* Specify ECMAScript target version: 'ES3' (default), 'ES5', 'ES2015', 'ES2016', 'ES2017','ES2018' or 'ESNEXT'. */,
+      module:
+        "commonjs" /* Specify module code generation: 'none', 'commonjs', 'amd', 'system', 'umd', 'es2015', or 'ESNext'. */,
+      outDir: "dist" /* Redirect output structure to the directory. */,
+      noEmitOnError: true /* Do not emit outputs. */,
+      strict: true /* Enable all strict type-checking options. */,
+      esModuleInterop: true /* Enables emit interoperability between CommonJS and ES Modules via creation of namespace objects for all imports. Implies 'allowSyntheticDefaultImports'. */
     }
   };
 
@@ -47,6 +55,6 @@ function createTsConfig(): void {
 
 function tsConfigExists(): boolean {
   return fs.existsSync(tsConfigPath);
-};
+}
 
-export default initialiseTypescriptApp
+export default initialiseTypescriptApp;
